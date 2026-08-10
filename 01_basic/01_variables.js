5f88c30733e95a75fe2d33cc0addc4fe6200bf4a
Shibsankar2004 <shibsankardandi32@gmail.com>
2026-08-10 05:32:33 +0000

    container confict done

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -8,5 +8,9 @@ accountcity="bangaluru"
  accountEmail="dandi@gmail.com"
  accountPassword="13346"
  accountcity="delhi"
+ /*
+prefer not to use var
+because of issue in block scope and functional scope
+ */
 
  console.table([accountId,accountEmail,accountPassword,accountState,accountcity])
