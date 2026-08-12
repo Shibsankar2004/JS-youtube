be0ed0b0016930a9f2a5a69100c17092d43c2260
Shibsankar2004 <shibsankardandi32@gmail.com>
2026-08-12 06:38:53 +0000

    String

---
+++ 01_basic/05_String.js
@@ -0,0 +1,40 @@
+const name="Shib"
+const gameName=new String('shib-Sankar-com')
+const repoCount=50
+// console.log(name+repoCount+" value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);//mostly use
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('i'));
+
+// const newString=gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString=gameName.slice(-10,8)
+// console.log(anotherString);
+
+
+const newStringOne="  shib   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url="https://shib.com/guhuuh%20uhuhb"
+console.log(url.replace('%20','-'));
+console.log(url.includes('sundar'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
