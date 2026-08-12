4ea4a96051bf7287f079d692c045e33c0fc8979d
Shibsankar2004 <shibsankardandi32@gmail.com>
2026-08-12 12:30:56 +0000

    date_time

---
+++ 01_basic/07_date.js
@@ -0,0 +1,41 @@
+let myDate=new Date()
+/*console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toTimeString());
+console.log(myDate.toUTCString());
+
+console.log(myDate.getTimezoneOffset());
+
+console.log(typeof myDate);*/
+//  let myCreateDate=new Date(2026,0,23)
+// let myCreateDate=new Date(2026,0,23,5,4)
+//  console.log(myCreateDate.toDateString());
+let myCreateDate=new Date("2026-01-17")
+//  console.log(myCreateDate.toLocaleString());
+
+let myTimeStamp= Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+newDate.toLocaleString('default',{
+    weekday:"long",
+    
+})
+
+
+
+
+
+ 
+
+
